7e07f2029a4dda786d37e7ac3e3f523e6a8dddf2
matt <mattleary311@gmail.com>
2020-03-13 20:54:07 -0400

    adding pkgdown
    first draft of slides templates, mainly testing pkgdown build and deploy

---
+++ R/leary_presentation.R
@@ -0,0 +1,12 @@
+#' Custom xarignan presentation template
+#'
+#' @param None Template has all parameters set in the yaml header
+#'
+#' @details to use the leary_presentation template, simply start a new file and
+#'     select From Tempplate > Leary Presentation
+#'
+#' @export
+
+leary_presentation <- function(){
+
+}
