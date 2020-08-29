65311a31db3951fb07e178dde42cc12e8717b15c
matt l <33221468+mleary@users.noreply.github.com>
2020-08-28 22:06:48 -0400

    fix docs

--- run-checks.R
+++ run-checks.R
@@ -2,6 +2,8 @@
 # Pkgdown files automatically.
 
 devtools::document()
-rmarkdown::render("./README.Rmd", output_format = "github_document") # need to confirm this works as expected
+# rmarkdown::render("./README.Rmd", output_format = "github_document") # need to confirm this works as expected
+# knitr::knit("./README.Rmd") # need to fix, runs but creates odd ouput in see_pkgs function on readme page
+#!!!!!!!!!!!!!!!!!!!! Need to run Readme to make sure up to date !!!!!!!!!!!!!!!!!!!!
 pkgdown::build_site()
 devtools::check()
